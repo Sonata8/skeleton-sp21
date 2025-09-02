456aff0cd41292746d55b035424c9f12f4a55a91
Sonata8 <wuwenhao2007@gmail.com>
2025-09-02 20:30:57 +0800

    done with emptySpaceExists()

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -137,7 +137,14 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        int size = b.size();
+        for (int col = 0; col < size; col += 1) {
+            for (int row = 0; row < size; row += 1) {
+                if (b.tile(col, row) == null) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
